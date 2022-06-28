6d4fe02c96fe5b65cc549aa59e22f4d6cb9274ed
vagrant <eng.saad.najm@gmail.com>
2022-06-28 03:09:19 +0000

    initial commit

---
+++ reverse.js
@@ -0,0 +1,18 @@
+const reverseArg = function (argument){
+  let result = "";
+  for (let i = 0; i < argument.length; i++) {
+    for (let j = argument[i].length-1; j >=0; j--) {
+      result += argument[i][j]
+      
+    }
+    if (i !== argument.length-1) {
+      result += '\n'
+    }
+  }
+  return result
+}
+// console.log(reverseArg('hello goodbye'))
+// console.log(reverseArg('1 fish 2 fish'))
+const arguments = process.argv.slice(2);
+console.log(arguments);
+console.log(reverseArg(arguments));
